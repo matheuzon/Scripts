90eb1108fd4f5672178d0c19f3275f10cb224d33
matheuzon <40461708+matheuzon@users.noreply.github.com>
2018-08-22 16:41:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+var x;
+
+function createButton()
+{
+     var btn = document.createElement('BUTTON');
+     var lbl = document.createTextNode("CLICK ME");        
+     btn.appendChild(lbl);   
+     btn.onclick = function()
+     {
+        x = "black";
+     }
+     document.body.appendChild(btn);    
+}
+
+createButton();
+
+if (x == "black") {
+  document.write(x);
+} else {
+  document.write("outra cor");
+}
+
 document.write("<h1>Titulo feito com JS</h1>");
-document.write("<h2>Subtitulo JS</h2>");
-document.write("<p>Opcao 1</p>");
-document.write("<p>Opcao 2</p>");
+
+
