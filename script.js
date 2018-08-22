945592be75bb97baaa9e0fef3752a6917829c57a
matheuzon <40461708+matheuzon@users.noreply.github.com>
2018-08-22 16:27:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.write("<h1>Título feito com JS</h1>");
-document.write("<h2>Subtítulo JS</h2>");
-document.write("<p>Opção 1</p>");
-document.write("<p>Opção 2</p>");
+document.write("<h1>Titulo feito com JS</h1>");
+document.write("<h2>Subtitulo JS</h2>");
+document.write("<p>Opcao 1</p>");
+document.write("<p>Opcao 2</p>");
